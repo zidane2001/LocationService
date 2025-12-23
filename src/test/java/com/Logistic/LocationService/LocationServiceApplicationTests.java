6d774f09b341b397493b6398bc042b4bfaa05068
zidane2001 <zidane2001@github.com>
2025-12-23 17:01:53 +0100

    initialisation of LoccationSercice

---
+++ src/test/java/com/Logistic/LocationService/LocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Logistic.LocationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
